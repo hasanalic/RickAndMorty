1a9f55b27025e0df6b4ff72fba690cc993ffe01a
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-04-18 19:48:28 +0300

    added retrofit api

---
+++ app/src/main/java/com/hasanalic/rickandmorty/api/RetrofitAPI.kt
@@ -0,0 +1,22 @@
+package com.hasanalic.rickandmorty.api
+
+import com.hasanalic.rickandmorty.model.Location
+import com.hasanalic.rickandmorty.model.LocationResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RetrofitAPI {
+
+    @GET("location")
+    suspend fun getLocations(): Response<LocationResponse>
+
+    @GET("location/{id}")
+    suspend fun getSingleLocation(@Path("id") id: Int): Response<Location>
+
+    @GET("character/{id}")
+    suspend fun getSingleCharacter(@Path("id") id: Int): Response<com.hasanalic.rickandmorty.model.Character>
+
+    @GET("character/{ids}")
+    suspend fun getMultipleCharacters(@Path("ids") ids: String): Response<List<com.hasanalic.rickandmorty.model.Character>>
+}
---
+++ app/src/main/java/com/hasanalic/rickandmorty/model/Character.kt
@@ -0,0 +1,16 @@
+package com.hasanalic.rickandmorty.model
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String?,
+    val gender: String,
+    val origin: Location,
+    val location: Location,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String
+)
