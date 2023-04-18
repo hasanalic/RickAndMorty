434350e8be1c31eaaea16510fac2bf427aca6c0c
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-04-18 19:29:07 +0300

    list fragment added

--- app/src/main/java/com/hasanalic/rickandmorty/view/ListFragment.kt
+++ app/src/main/java/com/hasanalic/rickandmorty/view/ListFragment.kt
@@ -5,15 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.hasanalic.rickandmorty.databinding.FragmentDetailBinding
+import com.hasanalic.rickandmorty.databinding.FragmentListBinding
 
-class DetailFragment: Fragment() {
+class ListFragment: Fragment() {
 
-    private var _binding: FragmentDetailBinding? = null
+    private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentDetailBinding.inflate(inflater,container,false)
+        _binding = FragmentListBinding.inflate(inflater,container,false)
         return binding.root
     }
 
