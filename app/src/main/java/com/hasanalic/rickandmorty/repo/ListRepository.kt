542bfbf193e041fd93640d63ac1626ad38952fde
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-04-18 21:37:41 +0300

    added list repository

---
+++ app/src/main/java/com/hasanalic/rickandmorty/repo/ListRepository.kt
@@ -0,0 +1,12 @@
+package com.hasanalic.rickandmorty.repo
+
+import com.hasanalic.rickandmorty.model.Location
+import com.hasanalic.rickandmorty.model.LocationResponse
+import com.hasanalic.rickandmorty.util.Resource
+
+interface ListRepository {
+
+    suspend fun getLocationResponse(): Resource<LocationResponse>
+    suspend fun getSingleLocation(id: Int): Resource<Location>
+    suspend fun getMultipleCharacters(ids: String): Resource<List<com.hasanalic.rickandmorty.model.Character>>
+}
