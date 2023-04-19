a0d6c1da2af0088c7846fc1195cb3121d06a28db
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-04-19 03:18:52 +0300

    added kotlin extensions

---
+++ app/src/main/java/com/hasanalic/rickandmorty/util/Extensions.kt
@@ -0,0 +1,43 @@
+package com.hasanalic.rickandmorty.util
+
+import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.hasanalic.rickandmorty.R
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun Fragment.toast(msg: String) {
+    Toast.makeText(requireContext(),msg, Toast.LENGTH_SHORT).show()
+}
+
+// glide
+fun ImageView.downloadFromUrl(url: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.drawable.ic_broken_image)
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(this)
+}
+
+fun placeHolderProgressBar(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 8f
+        centerRadius = 40f
+        start()
+    }
+}
