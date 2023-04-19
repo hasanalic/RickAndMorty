1f14b51e43a9eb7035ed92c6d7435f8f80b62c41
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-04-20 01:32:24 +0300

    updated detail fragment

--- app/src/main/java/com/hasanalic/rickandmorty/view/DetailFragment.kt
+++ app/src/main/java/com/hasanalic/rickandmorty/view/DetailFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.hasanalic.rickandmorty.databinding.FragmentDetailBinding
+import com.hasanalic.rickandmorty.util.*
+import com.hasanalic.rickandmorty.viewmodel.DetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +17,8 @@ class DetailFragment: Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var viewModel: DetailViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentDetailBinding.inflate(inflater,container,false)
         return binding.root
@@ -21,8 +26,47 @@ class DetailFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(DetailViewModel::class.java)
 
-        // logic
+        arguments?.let {
+            val characterID = DetailFragmentArgs.fromBundle(it).selectedCharacter
+            viewModel.getCharacter(characterID)
+        }
+
+        observer()
+    }
+
+    private fun observer() {
+        viewModel.character.observe(viewLifecycleOwner) {
+            when(it.status) {
+                Status.SUCCESS -> {
+                    binding.progressBarDetail.hide()
+                    var episodes: String = ""
+                    it.data?.episode?.map { episode ->
+                        episodes = episodes + episode.substringAfter("https://rickandmortyapi.com/api/episode/") + ", "
+                    }
+                    episodes = episodes.removeSuffix(", ")
+                    // iu
+                    binding.textViewTitle.text = it.data?.name?:"null"
+                    binding.imageViewCharacter.downloadFromUrl(it.data?.image?:"",
+                        placeHolderProgressBar(requireContext())
+                    )
+                    binding.textViewStatusText.text = it.data?.status?:"null"
+                    binding.textViewSpecyText.text = it.data?.species?:"null"
+                    binding.textViewGenderText.text = it.data?.gender?:"null"
+                    binding.textViewOriginText.text = it.data?.origin?.name?:"null"
+                    binding.textViewLocationText.text = it.data?.location?.name?:"null"
+                    binding.textViewEpisodesText.text = episodes
+                    binding.textViewCreatedAtText.text = it.data?.created?:"null"
+                }
+                Status.ERROR -> {
+                    binding.progressBarDetail.hide()
+                }
+                Status.LOADING -> {
+                    binding.progressBarDetail.show()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
