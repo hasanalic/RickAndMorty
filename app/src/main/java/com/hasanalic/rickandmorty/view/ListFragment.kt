c68e7738434df6fbee8c1a581757126832ec3ebb
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-04-20 01:30:56 +0300

    updated list fragment

--- app/src/main/java/com/hasanalic/rickandmorty/view/ListFragment.kt
+++ app/src/main/java/com/hasanalic/rickandmorty/view/ListFragment.kt
@@ -5,7 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hasanalic.rickandmorty.adapter.CharacterAdapter
+import com.hasanalic.rickandmorty.adapter.LocationAdapter
 import com.hasanalic.rickandmorty.databinding.FragmentListBinding
+import com.hasanalic.rickandmorty.util.Status
+import com.hasanalic.rickandmorty.util.hide
+import com.hasanalic.rickandmorty.util.show
+import com.hasanalic.rickandmorty.viewmodel.ListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +23,16 @@ class ListFragment: Fragment() {
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var viewModel: ListViewModel
+
+    private val locationAdapter by lazy {
+        LocationAdapter()
+    }
+
+    private val characterAdapter by lazy {
+        CharacterAdapter()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentListBinding.inflate(inflater,container,false)
         return binding.root
@@ -21,8 +40,83 @@ class ListFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(ListViewModel::class.java)
 
-        // logic
+        // Adapters
+        binding.recyclerViewLocations.adapter = locationAdapter
+        binding.recyclerViewLocations.layoutManager = LinearLayoutManager(requireContext(),
+            LinearLayoutManager.HORIZONTAL,false)
+        binding.recyclerViewCharacters.adapter = characterAdapter
+        binding.recyclerViewCharacters.layoutManager = LinearLayoutManager(requireContext(),
+            LinearLayoutManager.VERTICAL,false)
+
+        locationAdapter.setOnItemClickListener {
+            viewModel.getSingleLocation(it)
+        }
+        characterAdapter.setOnItemClickListener {
+            val action = ListFragmentDirections.actionListFragmentToDetailFragment(it)
+            Navigation.findNavController(view).navigate(action)
+        }
+        viewModel.getLocations()
+        observer()
+    }
+
+    private fun observer() {
+        viewModel.locations.observe(viewLifecycleOwner) {
+            when(it.status) {
+                Status.SUCCESS -> {
+                    binding.progressBarList.hide()
+                    binding.textViewError.hide()
+                    val locations = it.data?.locations ?: arrayListOf()
+                    locationAdapter.locations = locations
+                }
+                Status.ERROR -> {
+                    binding.progressBarList.hide()
+                    binding.textViewError.show()
+                }
+                Status.LOADING -> {
+                    binding.progressBarList.show()
+                    binding.textViewError.hide()
+                }
+            }
+        }
+        viewModel.singleLocation.observe(viewLifecycleOwner) {
+            when(it.status) {
+                Status.SUCCESS -> {
+                    binding.progressBarList.hide()
+                    binding.textViewError.hide()
+                    locationAdapter.changeLocation(it.data ?: -1)
+                }
+                Status.ERROR -> {
+                    binding.progressBarList.hide()
+                    binding.textViewError.text = "${it.message}! Try again."
+                    binding.textViewError.show()
+                }
+                Status.LOADING -> {
+                    binding.progressBarList.show()
+                    binding.textViewError.hide()
+                }
+            }
+        }
+        viewModel.characters.observe(viewLifecycleOwner) {
+            when(it.status) {
+                Status.SUCCESS -> {
+                    binding.progressBarList.hide()
+                    binding.textViewError.hide()
+                    val characters = it.data ?: arrayListOf()
+                    characterAdapter.characters = characters
+                }
+                Status.ERROR -> {
+                    binding.progressBarList.hide()
+                    binding.textViewError.text = "${it.message}! Try again."
+                    binding.textViewError.show()
+                }
+                Status.LOADING -> {
+                    binding.progressBarList.show()
+                    binding.textViewError.hide()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
