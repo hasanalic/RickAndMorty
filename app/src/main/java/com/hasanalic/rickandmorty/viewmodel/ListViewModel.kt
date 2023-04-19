90c0262c0c5bcbc08a4567b1ab3907cb43bc044f
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-04-19 22:16:05 +0300

    updated ListViewModel.kt

--- app/src/main/java/com/hasanalic/rickandmorty/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/hasanalic/rickandmorty/viewmodel/ListViewModel.kt
@@ -1,8 +1,65 @@
 package com.hasanalic.rickandmorty.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hasanalic.rickandmorty.model.LocationResponse
+import com.hasanalic.rickandmorty.repo.ListRepository
+import com.hasanalic.rickandmorty.util.Resource
+import com.hasanalic.rickandmorty.util.Status
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class ListViewModel: ViewModel() {
+class ListViewModel @Inject constructor(
+    private val repository: ListRepository
+): ViewModel() {
+
+    // Location list
+    private var _locations = MutableLiveData<Resource<LocationResponse>>()
+    val locations: LiveData<Resource<LocationResponse>>
+        get() = _locations
+
+    // Single location
+    private var _singleLocation = MutableLiveData<Resource<Int>>()
+    val singleLocation: LiveData<Resource<Int>>
+        get() = _singleLocation
+
+    // Character list
+    private var _characters = MutableLiveData<Resource<List<com.hasanalic.rickandmorty.model.Character>>>()
+    val characters: LiveData<Resource<List<com.hasanalic.rickandmorty.model.Character>>>
+        get() = _characters
+
+    fun getLocations() {
+        _locations.value = Resource.loading(null)
+        viewModelScope.launch {
+            val response = repository.getLocationResponse()
+            _locations.value = response
+        }
+    }
+
+    fun getSingleLocation(id: Int) {
+        _singleLocation.value = Resource.loading(null)
+        viewModelScope.launch {
+            val response = repository.getSingleLocation(id)
+            var ids: String = ""
+            if (response.status == Status.SUCCESS) {
+                _singleLocation.value = Resource.success(response.data?.id)
+                response.data?.residents?.map {
+                    ids = ids + it.substringAfter("https://rickandmortyapi.com/api/character/") + ","
+                }
+            }
+            getCharacters(ids)
+        }
+    }
+
+    private fun getCharacters(ids: String) {
+        _characters.value = Resource.loading(null)
+        viewModelScope.launch {
+            val response = repository.getMultipleCharacters(ids)
+            _characters.value = response
+        }
+    }
 }
